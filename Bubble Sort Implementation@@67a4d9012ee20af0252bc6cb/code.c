958dbf7547d92844988da6940ea72a50b9f64f6c
Agrim Gupta <agrim3368@gmail.com>
2025-03-31 23:51:04 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,48 +1,16 @@
-#include <stdio.h>
-
-void bubbleSort(int arr[], int n) {
-    int swapped;
-    for (int i = 0; i < n - 1; i++) {
-        swapped = 0; 
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
+void bubbleSort(int arr[], int n){
+    for (int i = 0; i < n-1; i++){
+        for (int j = 0; j < n-i-1; j++){
+            if (arr[j] > arr[j+1]){
                 int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-                swapped = 1; 
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
-        if (!swapped) break; 
     }
 }
-
-void printArray(const int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+void printArray(int arr[], int n){
+    for (int i = 0; i < n; i++){
+        printf("%d ",arr[i]);
     }
-    printf("\n");
-}
-
-int main() {
-    int n;
-    printf("Enter the number of elements: ");
-    if (scanf("%d", &n) != 1 || n <= 0) {
-        printf("Invalid input! Please enter a positive integer.\n");
-        return 1; 
-    }
-
-    int arr[n];
-    printf("Enter %d elements:\n", n);
-    for (int i = 0; i < n; i++) {
-        if (scanf("%d", &arr[i]) != 1) {
-            printf("Invalid input! Please enter integers only.\n");
-            return 1; 
-        }
-    }
-
-    bubbleSort(arr, n);
-    printf("Sorted array: ");
-    printArray(arr, n);
-
-    return 0;
 }
